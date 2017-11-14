41dc34af04ce93f8adade4fc535239f957b96be8
IldarAbdullin <wud_i@mail.ru>
2017-11-14 15:57:19 +0400

    Коммит 1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader("New.txt"))
+            {
+                var line = streamReader.ReadLine();
+                Console.WriteLine(line);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        public class Panda
+        {
+            public string Name { get; set; } = "Petya";
+            public int Age { get; set; } = 2;
+            public string Gender { get; set; } = "Male";
+
+        }
+
+        static void Main(string[] args)
+        {
+            var panda = new Panda();
+            var serialized = JsonConvert.SerializeObject(panda);
+            using (var streamWriter = new StreamWriter("New.txt"))
+            {
+                streamWriter.WriteLine(serialized);
+            }
+
+
+        }
+    }
+}
